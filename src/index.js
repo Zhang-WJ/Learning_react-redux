4a7efe370269b53bb03574bc35bb0cd10efa9767
zhangweijun <37130869+Zhang-WJ@users.noreply.github.com>
2018-09-30 11:47:49 +0800

    first refactory
    pure Redux only!

--- src/index.js
+++ src/index.js
@@ -1,27 +1,88 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
+import { TODO_ADD, TODO_TOGGLE, SHOW_ALL } from "./const";
 
 import "./styles.css";
+const FILTER_SET = "FILTER_SET";
 
-const store = createStore(reducer, []);
+const initialState = {
+  currentUser: null,
+  todos: [],
+  filter: "SHOW_ALL"
+};
+const rootReducer = combineReducers({
+  todoReducer: todoReducer,
+  filterState: filterReducer
+});
+const store = createStore(rootReducer);
+
+console.log("initial state:");
+console.log(store.getState());
+
+// Start subscribe the store's behavie
 const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 });
-store.dispatch({
-  type: "TODO_ADD",
-  todo: { id: "0", name: "learn redux", completed: false }
-});
 
+//Action creator -- return a plain action
+function doAddTodo(id, name) {
+  return {
+    type: TODO_ADD,
+    payload: {
+      todo: { id, name }
+    }
+  };
+}
+
+function doToggleTodo(id) {
+  return {
+    type: TODO_TOGGLE,
+    payload: {
+      todo: { id }
+    }
+  };
+}
+
+function doSetFilter(filter) {
+  return {
+    type: FILTER_SET,
+    payload: {
+      filter
+    }
+  };
+}
+
+// replace the parameter plain Action with an Action creator
+store.dispatch(doAddTodo("0", "Learn Redux"));
+store.dispatch(doAddTodo("1", "Learn Mobx"));
+store.dispatch(doToggleTodo("0"));
+store.dispatch(doSetFilter("COMPLETED"));
+// Stop subscribe
 unsubscribe();
-function reducer(state, action) {
+
+// Reducer is pure function return an object which hanle the state carefully
+function todoReducer(state = [], action) {
+  const { payload } = action;
   switch (action.type) {
-    case "TODO_ADD": {
-      return applyAddTodo(state, action);
+    case TODO_ADD: {
+      return applyAddTodo(state, payload);
     }
 
-    case "TODO_TOGGLE": {
-      return applyToggleTodo(state, action);
+    case TODO_TOGGLE: {
+      return applyToggleTodo(state, payload);
+    }
+
+    default:
+      return state;
+  }
+}
+
+function filterReducer(state = SHOW_ALL, action) {
+  const { payload } = action;
+  switch (action.type) {
+    case FILTER_SET: {
+      return applySetFilter(state, payload);
     }
 
     default:
@@ -30,15 +91,22 @@ function reducer(state, action) {
 }
 
 function applyAddTodo(state, action) {
-  return state.concat(action.todo);
+  // minimum Action payload
+  const todo = Object.assign({}, action.todo, { completed: false });
+  return state.concat(todo);
 }
 
 function applyToggleTodo(state, action) {
-  return state.map(todo => {
-    todo.id === action.todo.id
-      ? Object.assign({}, todo, { completed: !todo.completed })
-      : todo;
-  });
+  return state.map(
+    todo =>
+      todo.id === action.todo.id
+        ? Object.assign({}, todo, { completed: !todo.completed })
+        : todo
+  );
+}
+
+function applySetFilter(state, action) {
+  return action.filter;
 }
 
 const rootElement = document.getElementById("root");
